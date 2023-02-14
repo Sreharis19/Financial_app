fdf361b4fea098c030560288172358a69e5d498a
sreeharis19 <sreeharis19@gmail.com>
2023-02-14 23:56:07 +0000

    code refactoring completed

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,18 +4,6 @@ use App\Models\AuthModel;
 
 class Home extends BaseController
 {
-	
-	protected $session;
-
-
-    function __construct()
-    {
-
-        $this->session = \Config\Services::session();
-		$validation =  \Config\Services::validation();
-
-    }
-
 	public function index()
 	{
 		$data['title'] = "Login";
